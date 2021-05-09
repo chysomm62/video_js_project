f0e8bc330797219318266c321239b48689ae7508
chysomm62 <68497765+chysomm62@users.noreply.github.com>
2021-05-09 14:19:21 +0100

    worked on the video, header and switch

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 // MDN
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+
+const switchBtn = document.querySelector('.switch-btn');
+const video = document.querySelector('.video-container');
+
+switchBtn.addEventListener('click', () => {
+    if (!switchBtn.classList.contains("slide")){
+        switchBtn.classList.add("slide");
+        video.pause();
+    } 
+    else {
+        switchBtn.classList.remove("slide");
+        video.play();
+    }
+})
